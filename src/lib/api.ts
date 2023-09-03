b90398dceedeea6adfd0d0f3e7af5da897499bc5
Fabricio <fabriciolak12@gmail.com>
2023-09-03 13:40:19 -0300

    feat: add utils to lib

---
+++ src/lib/api.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+
+const instance = axios.create({
+  baseURL: 'https://api.unsplash.com/',
+  params: {
+    per_page: 4,
+  },
+  headers: {
+    'Accept-Version': 'v1',
+    Authorization: `Client-ID ${process.env.UNSPLASH_API_ACCESS_KEY}`,
+  },
+})
+
+const api = (axios: AxiosInstance) => {
+  return {
+    get: <T>(url: string, config: AxiosRequestConfig = {}) => {
+      return axios.get<T>(url, config)
+    },
+
+    post: <T>(url: string, config: AxiosRequestConfig = {}) => {
+      return axios.get<T>(url, config)
+    },
+  }
+}
+
+export default api(instance)
